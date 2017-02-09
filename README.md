06aa47f1e8f048a98bcc732bf12d5e613c858604
Peng Zhong <peng@nylira.com>
2017-02-09 15:14:32 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # vue-form-msg
 Form input errors and descriptions for Vue 2.
+
+## Installation
+
+    npm install @nylira/vue-form-msg
+
+## Usage
+
+    <template>
+      <form-msg name="Display Name" type="required" v-if="!$v.fields.displayName.required"></form-msg>
+      <form-msg name="Display Name" type="length" min="2" max="20" v-if="!$v.fields.displayName.menLength || !$v.fields.displayName.maxLength"></form-msg>
+    </template>
+
+    <script>
+      import FormMsg from '@nylira/vue-form-msg'
+      export default {
+        components: {
+          FormMsg
+        }
+      }
+    </script>
+
+    <style>
+      .ni-form-msg {
+        font-size 1.25rem;
+      }
+    </style>
+
+## Props
+TODO! For now, read the `./src/index.vue` file.
+
---
+++ src/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div :class="cssClass">
+    <template v-if="body">{{ body }}</template>
+    <template v-else>{{ name }} {{ error }}</template>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    cssClass () {
+      let value = 'ni-form-msg'
+      if (this.body) {
+        value += ' ni-form-msg-desc'
+      } else {
+        value += ' ni-form-msg-error'
+      }
+      return value
+    },
+    error () {
+      let msg = ''
+      switch (this.type) {
+        case 'required':
+          msg = 'is required'; break
+        case 'match':
+          msg = 'must be identical'; break
+        case 'minLength':
+          msg = `must be longer than ${this.min} characters`; break
+        case 'maxLength':
+          msg = `must be shorter than ${this.max} characters`; break
+        case 'length':
+          msg = `must be between ${this.min} and ${this.max} characters`; break
+        case 'between':
+          msg = `must be between ${this.min} and ${this.max}`; break
+        default:
+          msg = 'must be valid'; break
+      }
+      return msg
+    }
+  },
+  props: ['type', 'body', 'name', 'min', 'max']
+}
+</script>
+
+<style src="./style.css"></style>
